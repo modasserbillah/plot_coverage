409a6037f116009fa2dcba98d8def0e46f15f330
Modasser Billah <imtishad@gmail.com>
2020-06-25 21:44:20 +0600

    Bump version: 0.1.3 → 0.1.4

--- plot_coverage/__init__.py
+++ plot_coverage/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Modasser Billah"""
 __email__ = 'imtishad@gmail.com'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/modasserbillah/plot_coverage',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
