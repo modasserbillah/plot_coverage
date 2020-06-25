06275fc9b03f2c9c10430ecf71ab85e5987c808d
Modasser Billah <imtishad@gmail.com>
2020-06-25 22:42:38 +0600

    Bump version: 0.1.9 → 1.0.0

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/modasserbillah/plot_coverage',
-    version='0.1.9',
+    version='1.0.0',
     zip_safe=False,
 )
